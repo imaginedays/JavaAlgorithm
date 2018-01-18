19c4e0eb57ade1b2bb6edb26d0a6ce9c3c11b861
imaginedays <imaginedays@gmail.com>
2018-01-18 15:10:15 +0800

    1.最大公约数 2.二分查找法

--- lib/src/main/java/com/algorithm/imaginedays/lib/base/BinarySearch.java
+++ lib/src/main/java/com/algorithm/imaginedays/lib/base/BinarySearch.java
@@ -3,6 +3,7 @@ package com.algorithm.imaginedays.lib.base;
 /**
  * Created by imaginedays on 18/01/2018.
  * 二分查找
+ * 1.必须采用顺序存储结构。2.必须按关键字大小有序排列。
  */
 
 public class BinarySearch {
--- lib/src/main/java/com/algorithm/imaginedays/lib/myClass.java
+++ lib/src/main/java/com/algorithm/imaginedays/lib/myClass.java
@@ -12,11 +12,12 @@ public class myClass {
         out.println("hello world");
         /*欧几里德 求最大公约数*/
         int num = GCDSample.gcd(100,25);
-        out.print(num);
+        out.println(num);
+
         /*二分查找法*/
-        int[] intArray = new int[]{1,2,3,4,5,6,7,8,9,10,11,23};
-        final int rank = BinarySearch.rank(8 ,intArray);
-        out.println("rank = " + rank);
+        int[] sortIntArray = new int[]{1,2,3,4,5,6,7,8,9,10,11,23};
+        final int rank = BinarySearch.rank(8 ,sortIntArray);
+        out.println("sortIntArray[" + rank + "] = 8");
 
 
 
